abf57bbc0507c301b8d199e7eb56d881e70becae
chauncey <cchauncey@gmail.com>
2013-09-22 17:18:01 -0500

    Modified layout and cleaned up css

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,28 @@ $(document).ready(function(){
    $('.curtains').curtain();
 
 	var scrollorama = $.scrollorama({
-		blocks:'.curtains'
+		blocks:'.curtains',
+    enablePin:false
 	});
 
-    scrollorama.animate('#intro h1',{
-    	duration:200, property:'left', end:-860
-    });
+  scrollorama.animate('#intro h1',{
+    duration:100, property:'left', end:-1660
+  });
+
+  scrollorama.animate('#stuff ul li:nth-child(1)', {
+    delay: 780, duration: 60, property:'opacity', start:0
+  });
+
+  scrollorama.animate('#stuff ul li:nth-child(2)', {
+    delay: 800, duration: 60, property:'opacity', start:0
+  });
+
+  scrollorama.animate('#stuff ul li:nth-child(3)', {
+    delay: 820, duration: 60, property:'opacity', start:0
+  });
+
+  scrollorama.animate('#stuff ul li:nth-child(4)', {
+    delay: 840, duration: 60, property:'opacity', start:0
+  });
 
 });
